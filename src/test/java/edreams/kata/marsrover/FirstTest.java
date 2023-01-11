046c7eabfeb33166f4d000fd9450acdbc05f8e5b
Walter Nuccio <walter.nuccio@edreamsodigeo.com>
2023-01-11 15:46:22 +0100

    add maven support and first test

---
+++ src/test/java/edreams/kata/marsrover/FirstTest.java
@@ -0,0 +1,13 @@
+package edreams.kata.marsrover;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class FirstTest {
+    @Test
+    void walter_first_test() {
+        assertEquals(1, 1);
+    }
+}
