c31b04681328f1a6fc3cb4983926c14e18207153
aAnahit <anahit.arshakyan@odigeo.com>
2023-02-08 14:51:03 +0100

    Test push

--- src/test/java/edreams/kata/marsrover/RoverTest.java
+++ src/test/java/edreams/kata/marsrover/RoverTest.java
@@ -38,15 +38,33 @@ public class RoverTest {
         assertEquals("0:2:N", position);
     }
 
-
     @Test
     void ten_movements_forward_should_go_back_to_start_position() {
-        String steps = "MMMMMMMMMM";
+        String steps = generateString("M", 10);
         Rover rover = new Rover();
 
         String position = rover.move(steps);
 
         assertEquals("0:0:N", position);
+    }
+
+    @Test
+    void more_than_ten_movements_forward_wraps_around_the_grid() {
+        String steps = generateString("M", 12);
+        Rover rover = new Rover();
+
+        String position = rover.move(steps);
+
+        assertEquals("0:2:N", position);
+    }
+
+    private String generateString(String symbol, int lengthS) {
+        StringBuilder steps = new StringBuilder(symbol);
+        while (steps.length() < lengthS) {
+            steps.append(symbol);
+        }
+
+        return steps.toString();
     }
 
 }
