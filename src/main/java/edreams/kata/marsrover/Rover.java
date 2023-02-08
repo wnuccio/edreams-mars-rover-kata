9d6e1bc099231475c89093cbb0b8fc77857f01c5
aAnahit <anahit.arshakyan@odigeo.com>
2023-02-08 14:59:11 +0100

    Test push

--- src/main/java/edreams/kata/marsrover/Rover.java
+++ src/main/java/edreams/kata/marsrover/Rover.java
@@ -2,8 +2,10 @@ package edreams.kata.marsrover;
 
 public class Rover {
 
+    public static final int GRID_LENGTH = 10;
+
     public String move(String steps) {
-        return "0:" + steps.length() % 10 + ":N";
+        return "0:" + steps.length() % GRID_LENGTH + ":N";
     }
 
 }
