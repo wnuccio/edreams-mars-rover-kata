27c414460b2ad303370410fe7b17870591f74967
alexandre.souza <alexandre.souza@edreamsodigeo.com>
2023-03-02 16:03:23 +0100

    Refactoring tests

--- src/test/java/edreams/kata/marsrover/RoverTest.java
+++ src/test/java/edreams/kata/marsrover/RoverTest.java
@@ -1,5 +1,6 @@
 package edreams.kata.marsrover;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -8,32 +9,27 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class RoverTest {
 
+    private Rover rover;
+
     @Test
     void no_movement_forward() {
-        String commands = "";
-        Rover rover = new Rover();
 
-        String position = rover.move(commands);
+        String position = rover.move("");
 
         assertEquals("0:0:N", position);
     }
 
     @Test
     void one_movement_forward() {
-        String commands = "M";
-        Rover rover = new Rover();
-
-        String position = rover.move(commands);
+        String position = rover.move("M");
 
         assertEquals("0:1:N", position);
     }
 
     @Test
     void two_movements_forward() {
-        String commands = "MM";
-        Rover rover = new Rover();
 
-        String position = rover.move(commands);
+        String position = rover.move("MM");
 
         assertEquals("0:2:N", position);
     }
@@ -41,7 +37,6 @@ public class RoverTest {
     @Test
     void ten_movements_forward_should_go_back_to_start_position() {
         String commands = generateString("M", 10);
-        Rover rover = new Rover();
 
         String position = rover.move(commands);
 
@@ -51,7 +46,6 @@ public class RoverTest {
     @Test
     void more_than_ten_movements_forward_wraps_around_the_grid() {
         String commands = generateString("M", 12);
-        Rover rover = new Rover();
 
         String position = rover.move(commands);
 
@@ -61,7 +55,6 @@ public class RoverTest {
     @Test
     void change_direction_to_left() {
         String command = "L";
-        Rover rover = new Rover();
 
         String position = rover.move(command);
 
@@ -71,7 +64,6 @@ public class RoverTest {
     @Test
     void change_direction_to_left_two_times() {
         String command = "LL";
-        Rover rover = new Rover();
 
         String position = rover.move(command);
 
@@ -81,7 +73,6 @@ public class RoverTest {
     @Test
     void change_direction_to_left_three_times() {
         String command = "LLL";
-        Rover rover = new Rover();
 
         String position = rover.move(command);
 
@@ -91,7 +82,6 @@ public class RoverTest {
     @Test
     void change_direction_left_four_times() {
         String command = "LLLL";
-        Rover rover = new Rover();
 
         String position = rover.move(command);
 
@@ -101,7 +91,6 @@ public class RoverTest {
     @Test
     void change_direction_left_five_times() {
         String command = "LLLLL";
-        Rover rover = new Rover();
 
         String position = rover.move(command);
 
@@ -111,7 +100,6 @@ public class RoverTest {
     @Test
     void change_direction_to_right() {
         String command = "R";
-        Rover rover = new Rover();
 
         String position = rover.move(command);
 
@@ -121,7 +109,6 @@ public class RoverTest {
     @Test
     void change_direction_to_right_two_times() {
         String command = "RR";
-        Rover rover = new Rover();
 
         String position = rover.move(command);
 
@@ -131,7 +118,6 @@ public class RoverTest {
     @Test
     void change_direction_to_right_three_times() {
         String command = "RRR";
-        Rover rover = new Rover();
 
         String position = rover.move(command);
 
@@ -141,7 +127,6 @@ public class RoverTest {
     @Test
     void change_direction_to_right_four_times() {
         String command = "RRRR";
-        Rover rover = new Rover();
 
         String position = rover.move(command);
 
@@ -151,7 +136,6 @@ public class RoverTest {
     @Test
     void change_direction_to_right_five_times() {
         String command = "RRRRR";
-        Rover rover = new Rover();
 
         String position = rover.move(command);
 
@@ -161,7 +145,6 @@ public class RoverTest {
     @Test
     void move_forward_and_rotate_left() {
         String command = "ML";
-        Rover rover = new Rover();
 
         String position = rover.move(command);
 
@@ -171,7 +154,6 @@ public class RoverTest {
     @Test
     void move_to_the_right() {
         String command = "RM";
-        Rover rover = new Rover();
 
         String position = rover.move(command);
 
@@ -181,17 +163,20 @@ public class RoverTest {
     @Test
     void move_to_east_and_come_back() {
         String command = "RMLLM";
-        Rover rover = new Rover();
 
         String position = rover.move(command);
 
         assertEquals("0:0:W", position);
     }
 
+    @BeforeEach
+    void setUp() {
+        rover = new Rover();
+    }
+
     @Test
     void move_to_left_and_move_forward() {
         String command = "LM";
-        Rover rover = new Rover();
 
         String position = rover.move(command);
 
