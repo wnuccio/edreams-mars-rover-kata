16eaa86825771ea551fae227ab895b944a0260e4
Davide Razzoli <davide.razzoli@edreamsodigeo.com>
2023-03-06 15:06:32 +0100

    Refactoring

--- src/test/java/edreams/kata/marsrover/RoverTest.java
+++ src/test/java/edreams/kata/marsrover/RoverTest.java
@@ -11,9 +11,13 @@ public class RoverTest {
 
     private Rover rover;
 
+    @BeforeEach
+    void setUp() {
+        rover = new Rover();
+    }
+
     @Test
     void no_movement_forward() {
-
         String position = rover.move("");
 
         assertEquals("0:0:N", position);
@@ -28,7 +32,6 @@ public class RoverTest {
 
     @Test
     void two_movements_forward() {
-
         String position = rover.move("MM");
 
         assertEquals("0:2:N", position);
@@ -54,131 +57,98 @@ public class RoverTest {
 
     @Test
     void change_direction_to_left() {
-        String command = "L";
-
-        String position = rover.move(command);
+        String position = rover.move("L");
 
         assertEquals("0:0:W", position);
     }
 
     @Test
     void change_direction_to_left_two_times() {
-        String command = "LL";
-
-        String position = rover.move(command);
+        String position = rover.move("LL");
 
         assertEquals("0:0:S", position);
     }
 
     @Test
     void change_direction_to_left_three_times() {
-        String command = "LLL";
-
-        String position = rover.move(command);
+        String position = rover.move("LLL");
 
         assertEquals("0:0:E", position);
     }
 
     @Test
     void change_direction_left_four_times() {
-        String command = "LLLL";
-
-        String position = rover.move(command);
+        String position = rover.move("LLLL");
 
         assertEquals("0:0:N", position);
     }
 
     @Test
     void change_direction_left_five_times() {
-        String command = "LLLLL";
-
-        String position = rover.move(command);
+        String position = rover.move("LLLLL");
 
         assertEquals("0:0:W", position);
     }
 
     @Test
     void change_direction_to_right() {
-        String command = "R";
-
-        String position = rover.move(command);
+        String position = rover.move("R");
 
         assertEquals("0:0:E", position);
     }
 
     @Test
     void change_direction_to_right_two_times() {
-        String command = "RR";
-
-        String position = rover.move(command);
+        String position = rover.move("RR");
 
         assertEquals("0:0:S", position);
     }
 
     @Test
     void change_direction_to_right_three_times() {
-        String command = "RRR";
-
-        String position = rover.move(command);
+        String position = rover.move("RRR");
 
         assertEquals("0:0:W", position);
     }
 
     @Test
     void change_direction_to_right_four_times() {
-        String command = "RRRR";
-
-        String position = rover.move(command);
+        String position = rover.move("RRRR");
 
         assertEquals("0:0:N", position);
     }
 
     @Test
     void change_direction_to_right_five_times() {
-        String command = "RRRRR";
-
-        String position = rover.move(command);
+        String position = rover.move("RRRRR");
 
         assertEquals("0:0:E", position);
     }
 
     @Test
     void move_forward_and_rotate_left() {
-        String command = "ML";
-
-        String position = rover.move(command);
+        String position = rover.move("ML");
 
         assertEquals("0:1:W", position);
     }
 
     @Test
     void move_to_the_right() {
-        String command = "RM";
-
-        String position = rover.move(command);
+        String position = rover.move("RM");
 
         assertEquals("1:0:E", position);
     }
 
     @Test
     void move_to_east_and_come_back() {
-        String command = "RMLLM";
-
-        String position = rover.move(command);
+        String position = rover.move("RMLLM");
 
         assertEquals("0:0:W", position);
     }
 
-    @BeforeEach
-    void setUp() {
-        rover = new Rover();
-    }
-
     @Test
     void move_to_left_and_move_forward() {
-        String command = "LM";
-
-        String position = rover.move(command);
+        String position = rover.move("LM");
 
         assertEquals("9:0:W", position);
     }
